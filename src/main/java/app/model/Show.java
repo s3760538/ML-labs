e39fb4f17c3a1ddad01515ab778ec968d33631e8
Peter Bui s3786794 <s3786794@student.rmit.edu.au>
2020-04-05 10:40:55 +1000

    Added getters and toString to Show.java for future implementation

--- src/main/java/app/model/Show.java
+++ src/main/java/app/model/Show.java
@@ -30,5 +30,53 @@ public class Show {
     	this.year = year;
     }
 
+    @Override
+    public String toString() {
+    	return String.format(
+    	"Show ID: %s\n" +
+    	"Show Title: %s\n" +
+    	"Length: %s\n" +
+    	"Movie/Series: %s\n" +
+    	"Genre: %s\n" +
+    	"Year: %s\n",
+    	getShowID(), getShowTitle(), getLength(), 
+    	getType(), getGenre(), getYear()
+    	);
+    }
+    
+    public String type() {
+    	String type = "";
+    	
+    	if (this.isMovie == true) {
+    		type = "Movie";
+    	} else if (this.isSeries == true) {
+    		type = "Series";
+    	}
+    	
+    	return type;
+    }
+    
+    public int getShowID() {
+    	return this.showid;
+    }
+    
+    public String getShowTitle() {
+    	return this.showTitle;
+    }
+    
+    public double getLength() {
+    	return this.length;
+    }
+    
+    public String getType() {
+    	return type();
+    }
+    
+    public String getGenre() {
+    	return genre;
+    }
 
+    public int getYear() {
+    	return this.year;
+    }
 }
